d4977620242dccd4a990b1b6c4dc0c5439f759d9
Shivam Mishra <codeshivam9430@gmail.com>
2025-11-13 10:02:32 +0530

    Fix typo in error handler status code property

--- middlewares/globalErrorHandler.js
+++ middlewares/globalErrorHandler.js
@@ -1,5 +1,5 @@
 export const globalErrorHandler = (err, req, res, next) => {
-  const statuCode = err.statuCode || 500;
+  const statuCode = err.statusCode || 500;
   res.status(statuCode).json({
     success: false,
     message: err.message || "Internal Server Error",
